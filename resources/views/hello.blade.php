f5f6244646e50341cb12669093275dc84d9d9c33
valentina676 <153191667+valentina676@users.noreply.github.com>
2024-06-06 09:28:00 -0300

    aula0606

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,1 @@
+<p>Meu primeiro arquivo blade :)</p>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function() {
+    return view ('Hello');
+});
+
+Route::get('/hello/{nome}', function($nome){
+    return "ola {$nome}";
+});
