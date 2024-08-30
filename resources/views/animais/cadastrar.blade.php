bdb2de1ff1d8c005f7351caa33d00bad14af183c
valentina676 <153191667+valentina676@users.noreply.github.com>
2024-08-30 11:29:32 -0300

    aula30/08

--- resources/views/animais/cadastrar.blade.php
+++ resources/views/animais/cadastrar.blade.php
@@ -8,20 +8,24 @@
 <p>Preencha o formulário</p>
 
 @if($errors->any())
-<div>
+<div >
     <h4>Deu ruim</h4>
     @foreach($errors-> all() as $erro)
         <p>{{$erro}}</p>
     @endforeach
 </div>
 @endif
-
-<form method="post" action="{{route('animais.gravar') }}">
+<div class="leading-loose">
+<form class="p-10 bg-white rounded shadow-xl" method="post" action="{{route('animais.gravar') }}">
     @csrf
-    <input type="text" name="nome" placeholder="Nome" value="{{old('nome')}}">
+    <label class="block text-sm text-gray-600" for="name">Name</label>
+    <input class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" type="text" name="nome" placeholder="Nome" value="{{old('nome')}}">
     <br>
-    <input type="number" name="idade" placeholder="Idade" value="{{old('idade')}}" >
+    <label class="block text-sm text-gray-600" for="name">Idade</label>
+    <input class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" type="number" name="idade" placeholder="Idade" value="{{old('idade')}}" >
     <br>
-    <input type="submit" value="Gravar">
+    <main class="w-full flex-grow p-6">
+    <input class="px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded"  type="submit" value="Gravar">
 </form>
+</div>
 @endsection
--- resources/views/animais/index.blade.php
+++ resources/views/animais/index.blade.php
@@ -11,20 +11,30 @@
 </p>
 <p> Veja nossa lista de clientes</p>
 
-    <table border="1">
+    <table class="min-w-full bg-white" border="1">
+        <thead class="bg-gray-800 text-white">
         <tr>
-            <th>Nome</th>
-            <th>Idade</th>
+            <th class="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">Nome</th>
+            <th class="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">Idade</th>
+            <th class="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">Apagar</th>
+            <th class="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">Editar</th>
         </tr>
-
+        </thead>
         @foreach($animais as $animal)
-        <tr>
-            <td>{{$animal['nome']}}</td>
-            <td>{{$animal['idade']}}</td>
-            <td><a href="{{route('animais.apagar', $animal['id'])}}">Apagar</a></td>
-            <td><a href="{{route('animais.editar', $animal['id'])}}">Editar</a></td>
+
+        <tbody class="text-gray-700">
+        <tr @if($loop->even) class="bg-gray-200" @endif>
+            <td class="text-left py-3 px-4">{{$animal['nome']}}</td>
+            <td class="text-left py-3 px-4">{{$animal['idade']}}</td>
+            <td class="text-left py-3 px-4"><a class="inline-block px-3 py-1 font-semibold text-green-900 leading-tight bg-red-400 opacity-100 rounded-full"href="{{route('animais.apagar', $animal['id'])}}">Apagar</a></td>
+            <td class="text-left py-3 px-4"><a class="inline-block px-3 py-1 font-semibold text-green-900 leading-tight bg-blue-400 opacity-100 rounded-full" href="{{route('animais.editar', $animal['id'])}}">Editar</a></td>
         </tr>
+        </tbody>
         @endforeach
     </table>
+
+    
+
+
 
 @endsection
